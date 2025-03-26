dacf3c4ba7d7542b349f2672821b8fcdbe5159ff
JApitpit <Jianpitpit@gmail.com>
2025-03-25 20:14:59 -0600

    changed supabase project

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://sbwczrrduwqhehkgpmxn.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNid2N6cnJkdXdxaGVoa2dwbXhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI4ODcyNTEsImV4cCI6MjA1ODQ2MzI1MX0.XieFcgxvuwcHSpNMGegZZZWsviF2J9euwq4GV6n7VA0';
+const SUPABASE_URL = 'https://nnqqfxcveilukqdrwxob.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ucXFmeGN2ZWlsdWtxZHJ3eG9iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI5NTE2MTQsImV4cCI6MjA1ODUyNzYxNH0.EJABGCHdYTtPUMywdTQEkRx-vigxHAhaCOiY3K1fP9I';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
